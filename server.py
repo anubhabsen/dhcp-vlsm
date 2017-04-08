e8a0f44876421a4d29eb699b257d0df211dd2d31
Sourabh Rajguru <srsoccerfan@gmail.com>
2017-04-08 23:01:54 +0530

    Unknown lab

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 lines = [line.rstrip('\n') for line in open('subnets.conf')]
+# print lines
 
 base_address = (lines[0].split('/'))[0]
 bits_available = 32 - int(lines[0].split('/')[1])
@@ -16,7 +17,7 @@ for i in range(2 + N, len(lines)):
 	temp = lines[i].split('-')
 	mac_to_lab[temp[0]] = temp[1]
 
-max_hosts = (1 << bits_available) - 2 * (N + 1)
+max_hosts = max((1 << bits_available) - 2 * (N + 1), 0)
 
 total_requests = 0
 
@@ -25,7 +26,6 @@ for i in range(2, 2 + N):
 
 while total_requests > max_hosts:
 	max_requests_from_lab = 0
-	index_of_max = -1
 	for key, value in lab_requests.iteritems():
 		if value > max_requests_from_lab:
 			max_requests_from_lab = value
@@ -34,3 +34,10 @@ while total_requests > max_hosts:
 	lab_requests[index_of_max] -= 1
 
 	total_requests -= 1
+
+# print bits_available
+# print max_hosts
+if max_hosts - total_requests > 0:
+	lab_requests['Unknown'] = max_hosts - total_requests
+
+print lab_requests
