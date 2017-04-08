cd83089ca214da012f50adf032e427c911a7b6c1
Anubhab Sen <anubhabsen@gmail.com>
2017-04-08 22:44:37 +0530

    reduces allocated hosts till < limit

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ lines = [line.rstrip('\n') for line in open('subnets.conf')]
 
 base_address = (lines[0].split('/'))[0]
 bits_available = 32 - int(lines[0].split('/')[1])
-print bits_available
 
 N = int(lines[1])
 
@@ -18,3 +17,20 @@ for i in range(2 + N, len(lines)):
 	mac_to_lab[temp[0]] = temp[1]
 
 max_hosts = (1 << bits_available) - 2 * (N + 1)
+
+total_requests = 0
+
+for i in range(2, 2 + N):
+	total_requests += int(lines[i].split(':')[1])
+
+while total_requests > max_hosts:
+	max_requests_from_lab = 0
+	index_of_max = -1
+	for key, value in lab_requests.iteritems():
+		if value > max_requests_from_lab:
+			max_requests_from_lab = value
+			index_of_max = key
+
+	lab_requests[index_of_max] -= 1
+
+	total_requests -= 1
